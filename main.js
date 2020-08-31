900eaf7831f82c792d963306819e2dd1fcb33a67
TiagoBauer <tiagopbauer@gmail.com>
2020-08-30 22:47:10 -0300

    [RocketSeat]Feat - Class types, creating, extending and using classes.

--- main.js
+++ main.js
@@ -1,11 +1,50 @@
 alert('teste');
 
 class Teste {
+    //First Class
     metodo(){
 
     }
 
     outroMetodo(){
-        
+
     }
 }
+
+class List {
+    constructor(){
+        this.data = [];
+    }
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+class ToDoList extends List {
+    constructor() {
+        super();
+
+        this.usuario = 'Tiago';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+const minhaLista = new ToDoList();
+
+document.getElementById('novoToDo').onclick = function(){
+    minhaLista.add('Novo To Do');
+}
+
+minhaLista.mostraUsuario();
+
+
+class Matematica {
+    static soma(a,b) {
+        return a + b;
+    }
+}
+console.log(Matematica.soma(1,2));
